dc7607174c9b28ac17956495c95cbeed33be85f3
oktay.tontas <oktay.tontas@albil.com.tr>
2022-02-23 22:35:56 +0300

    l

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-$('.thumbnail').click(function(){
+$('.thumbnail').click(function () {
     $('.modal-body').empty();
     $($(this).parents('div').html()).appendTo('.modal-body');
-    $('#modal').modal({show:true});
+    $('#modal').modal({ show: true });
 });
 
 $('#modal').on('show.bs.modal', function () {
@@ -11,3 +11,28 @@ $('#modal').on('show.bs.modal', function () {
 $('#modal').on('hide.bs.modal', function () {
     $('.col-6,.row .thumbnail').removeClass('blur');
 })
+
+
+let switcherLi = document.querySelectorAll(".switcher li");
+let imgs = Array.from(document.images);
+
+switcherLi.forEach((li) => {
+    li.addEventListener("click", removeActive);
+    li.addEventListener("click", filter);
+});
+
+function removeActive() {
+    switcherLi.forEach((li) => {
+        li.classList.remove("active");
+        this.classList.add("active");
+    });
+}
+
+function filter() {
+    imgs.forEach((img) => {
+        img.style.display = "none";
+    });
+    document.querySelectorAll(this.dataset.cat).forEach((el) => {
+        el.style.display = "block";
+    });
+}
